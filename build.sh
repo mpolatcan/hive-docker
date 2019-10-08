33dde3bfd1639d93546a6bb8f59e1d6c1789a6b3
mpolatcan <mutlupolatcan@gmail.com>
2019-10-08 10:21:40 +0300

    alpine versions removed due to lack of glibc

--- build.sh
+++ build.sh
@@ -14,24 +14,27 @@ HADOOP_VERSIONS=(
     "2.7.7"
 )
 
-DISTS=(
-    "alpine"
-    "ubuntu"
+JAVA_VERSIONS=(
+    "8"
+    "9"
+    "10"
+    "11"
+    "12"
 )
 
-# $1: DIST
-# $2: HIVE_VERSION
-# $3: HADOOP_VERSION
+# $1: HIVE_VERSION
+# $2: HADOOP_VERSION
+# $3: JAVA_VERSION
 function build_image() {
-  sudo docker build -q -t mpolatcan/hive:$1-$2-hadoop-$3 --build-arg HIVE_VERSION=$2 --build-arg HADOOP_VERSION=$3 ./$1/
-	sudo docker push mpolatcan/hive:$1-$2-hadoop-$3
-	sudo docker rmi mpolatcan/hive:$1-$2-hadoop-$3
+  sudo docker build -q -t mpolatcan/hive:$1-hadoop-$2-java$3 --build-arg HIVE_VERSION=$1 --build-arg HADOOP_VERSION=$2 --build-arg JAVA_VERSION=$3 ./src/
+	sudo docker push mpolatcan/hive:$1-hadoop-$2-java$3
+	sudo docker rmi mpolatcan/hive:$1-hadoop-$2-java$3
 }
 
 for HIVE_VERSION in ${HIVE_VERSIONS[@]}; do
     for HADOOP_VERSION in ${HADOOP_VERSIONS[@]}; do
-        for DIST in ${DISTS[@]}; do
-            build_image $DIST $HIVE_VERSION $HADOOP_VERSION
+        for JAVA_VERSION in ${JAVA_VERSIONS[@]}; do
+            build_image $HIVE_VERSION $HADOOP_VERSION $JAVA_VERSION
         done
     done
 done
