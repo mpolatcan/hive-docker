9e5da224752b2da19ed334d6f3efe693f1fcaf2f
mpolatcan <mutlupolatcan@gmail.com>
2019-10-10 15:26:37 +0300

    hive dockerfile fixed

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-function execCmd() { su - hduser -s /bin/bash -c "$1"; }
-
 function startHive() {
-    execCmd "${HADOOP_HOME}/bin/hadoop fs -mkdir -p /tmp"
-    execCmd "${HADOOP_HOME}/bin/hadoop fs -mkdir -p /user/hive/warehouse"
-    execCmd "${HADOOP_HOME}/bin/hadoop fs -chmod g+w /tmp"
-    execCmd "${HADOOP_HOME}/bin/hadoop fs -chmod g+w /user/hive/warehouse"
-    execCmd "${HIVE_HOME}/bin/schematool -dbType derby -initSchema"
+    hdfs dfs -mkdir -p /tmp
+    hdfs dfs -mkdir -p /user/hive/warehouse
+    hdfs dfs -chmod g+w /tmp
+    hdfs dfs -chmod g+w /user/hive/warehouse
+    schematool -dbType derby -initSchema
 }
 
 # Start Hadoop services with loaded configurations
