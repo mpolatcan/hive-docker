00a78272fdfeef5f765b85feaf51d97493ef660c
mpolatcan <mutlupolatcan@gmail.com>
2020-10-09 13:49:42 +0300

    hive metastore postgresql settings added

--- src/setup/entrypoint.sh
+++ src/setup/entrypoint.sh
@@ -1,21 +1,42 @@
 #!/usr/bin/env bash
-function start_hive() {
+# $1: message
+function __log__() {
+    echo "[$(date '+%d/%m/%Y %H:%M:%S')] -> $1"
+}
+
+function configure_hive() {
     hdfs dfs -mkdir -p /tmp
     hdfs dfs -mkdir -p /user/hive/warehouse
     hdfs dfs -chmod g+w /tmp
     hdfs dfs -chmod g+w /user/hive/warehouse
-    schematool -dbType derby -initSchema
 }
 
-# Start Hadoop services with loaded configurations
-./hadoop_entrypoint.sh $1
+function configure_metastore() {
+    if [[ ${HIVE_METASTORE_DB_TYPE} == "postgres" ]]; then
+        IFS='.' read -r -a HIVE_VERSION_TOKENS <<< "$HIVE_VERSION"
+        __log__ "Hive version is ${HIVE_VERSION_TOKENS[0]}.${HIVE_VERSION_TOKENS[1]}.0"
 
-# Load Hive configs
-./hive_config_loader.sh
+        psql postgresql://${POSTGRES_USER}:$/{POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT:=5432}/${POSTGRES_DB} -a \
+             -f ${HIVE_HOME}/scripts/metastore/upgrade/postgres/hive-schema-${HIVE_VERSION_TOKENS[0]}.${HIVE_VERSION_TOKENS[1]}.0.postgres.sql
+    fi
 
-# Start Hive with embedded Derby database
-start_hive
+    schematool -dbType ${HIVE_METASTORE_DB_TYPE:=derby} -initSchema
+}
 
-if [[ "$1" == "hive" ]]; then
-    tail -f /dev/null
-fi
+function main() {
+    # Start Hadoop services with loaded configurations
+    ./hadoop_entrypoint.sh $1
+
+    # Load Hive configs
+    ./hive_config_loader.sh
+
+    configure_hive
+
+    configure_metastore
+
+    if [[ "$1" == "hive" ]]; then
+        tail -f /dev/null
+    fi
+}
+
+main $1
